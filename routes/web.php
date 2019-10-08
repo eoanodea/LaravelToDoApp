9cf5e724e123990517e3e78baba1d10a6e3ae83d
Eoan O'Dea <odeaeoan@gmail.com>
2019-10-08 15:37:09 +0100

    Added login email verification and forgot password emails, along with user login system and authentication

--- routes/web.php
+++ routes/web.php
@@ -24,3 +24,7 @@ Route::get('/todos/{id}', 'TodoController@show')->name('todos.show');
 Route::get('/todos/{id}/edit', 'TodoController@edit')->name('todos.edit');
 Route::put('/todos/{id}', 'TodoController@update')->name('todos.update');
 Route::delete('/todos/{id}', 'TodoController@destroy')->name('todos.destroy');
+
+Auth::routes(['verify' => true]);
+
+Route::get('/home', 'HomeController@index')->name('home')->middleware('verified');
